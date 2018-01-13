a7fb6f4a53e1f7e4b58a83cd17d9040b811ffe8c
Unknown <clintyfresh163@yahoo.com>
2018-01-13 17:14:17 -0600

    made cat move

---
+++ Assets/CatMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMovement : MonoBehaviour {
+    public Rigidbody rigidbody;
+    float TurnTimer = 0;
+    float TurnRate = 0;
+    public float speed = 30;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+
+        TurnTimer -= Time.deltaTime;
+        if(TurnTimer < 0)
+        {
+            TurnTimer = Random.Range(0.5f, 3f);
+            TurnRate = Random.Range(-180, 180);
+        }
+
+        TurnAtRate(TurnRate);
+        rigidbody.AddForce(transform.forward * speed, ForceMode.Acceleration);
+	}
+
+    private void TurnAtRate(float rate)
+    {
+        Quaternion goal = new Quaternion();
+        goal = transform.rotation * Quaternion.Euler(0,rate/50,0);
+        rigidbody.MoveRotation(goal);
+        
+    }
+}
